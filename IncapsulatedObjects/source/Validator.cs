b8980ae914524e2bf4d2597492a2c74cae40fd20
Arkadiy Zakharov <69598137+Mebebonk@users.noreply.github.com>
2023-10-27 04:13:52 +0500

    updated ruleset

--- IncapsulatedObjects/source/Validator.cs
+++ IncapsulatedObjects/source/Validator.cs
@@ -6,7 +6,7 @@ namespace IncapsulatedObjects
 {
 	static public class Validator
 	{
-		public static bool ValidationTest(Coordinate tile)
+		static public bool ValidationTest(Coordinate tile)
 		{
 			return tile.X > 0 && tile.X < RuleSet.RuleSet.maxWidth && tile.Y > 0 && tile.Y < RuleSet.RuleSet.maxHight;
 		}
--- RuleSet/source/RuleSet.cs
+++ RuleSet/source/RuleSet.cs
@@ -2,14 +2,9 @@
 {
 	static public class RuleSet
 	{
-		public static readonly int maxWidth = 20;
-		public static readonly int maxHight = 20;
-		public static readonly int[] moveTime = 
-		{
-			500,
-			400,
-			350
-		};
-		public static readonly bool edgeTP = true;			
+		public const int maxWidth = 20;
+		public const int maxHight = 20;
+		public const int moveTime = 500;
+		public const bool edgeTP = true;			
 	}
 }
